da8dc1baac66e6756944f77c2c7605d1a88fd680
Justin Harding <justin.harding@midurth.com>
2019-02-17 18:59:34 +1300

    provide arguments for certificate authority name, website url etc

--- create_root_cert.sh
+++ create_root_cert.sh
@@ -4,14 +4,68 @@
 # https://jitsusamablog.wordpress.com/2016/08/03/self-signed-certificates-in-ios-simulator/
 # https://blog.kloud.com.au/2016/06/12/creating-openssl-self-signed-certs-on-windows/
 
-# create our CA private key
-openssl genrsa -out ca.key 2048
+usage() {
+  cat <<-END
+    Create a certificate authority to use for generating certificates
+    optionally create a windows pfx file for iis
 
-# use this key to encrypt a new CA certificate file
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 512 -out ca.crt \
-  -subj "/C=NZ/ST=Otago/L=Alexandra/O=Peaksoft/CN=Peaksoft CA"
+    Usage: $0 -a certificate authority name -c country code -s state -t town/city [-w]
+    e.g. $0 -a Peaksoft -c NZ -s Otago -t Alexandra -w
 
-# Pacakge your public and private key in a pkcs12 encripted file (to install with certmgr on windows)
-## ref> https://www.openssl.org/docs/manmaster/apps/pkcs12.html<Paste>
-openssl pkcs12 -export -inkey ca.key -in ca.crt -out ca.pfx
+    country, state and town are used for the certificate
+    -w  generate windows pfx file for iis. You will be prompted for a password to protect the file
+END
+  exit 1;
+}
+
+generate_pfx=false
+country="NZ"
+state="Otago"
+city="Alexandra"
+
+while getopts ":wa:c:s:t:" o; do
+  case "${o}" in
+    a)
+      authority=${OPTARG}
+      ;;
+    c)
+      country=${OPTARG}
+      ;;
+    s)
+      state=${OPTARG}
+      ;;
+    t)
+      city=${OPTARG}
+      ;;
+    w)
+      generate_pfx=true
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+if [ -z "${authority}" ]; then
+  cat <<-END
+    you must specify an authority
+
+END
+  usage
+fi
+
+lower_authority=$(echo $authority | awk '{print tolower($0)}')
+
+echo "creating CA private key into ${lower_authority}.key"
+openssl genrsa -out ${lower_authority}.key 2048
+
+echo "using new key to encrypt new CA certificate file ${lower_authority}.crt"
+openssl req -x509 -new -nodes -key ${lower_authority}.key -sha256 -days 512 -out ${lower_authority}.crt \
+  -subj "/C=${country}/ST=${state}/L=${city}/O=${authority}/CN=${authority} CA"
+
+if $generate_pfx; then
+  echo "packaging public and private key in pkcs12 encrypted file ${lower_authority}.pfx (to install with certmgr on windows)"
+  ## ref> https://www.openssl.org/docs/manmaster/apps/pkcs12.html<Paste>
+  openssl pkcs12 -export -inkey ${lower_authority}.key -in ${lower_authority}.crt -out ${lower_authority}.pfx
+fi
 
--- create_website_cert.sh
+++ create_website_cert.sh
@@ -4,20 +4,101 @@
 # https://jitsusamablog.wordpress.com/2016/08/03/self-signed-certificates-in-ios-simulator/
 # https://blog.kloud.com.au/2016/06/12/creating-openssl-self-signed-certs-on-windows/
 
-# create the key for a localhost server
-openssl genrsa -out dev.safely.local.key 2048
+usage() {
+  cat <<-END
+    Create a certificate suitable for a website using specified certificate authority
+    optionally create a windows pfx file for iis
 
-# Using this key, we next sign a CSR (Certificate Signing Request)
-openssl req -new -key dev.safely.local.key -out dev.safely.local.csr \
-  -subj "/C=NZ/ST=Otago/L=Alexandra/O=Peaksoft/CN=dev.safely.local"
+    Usage: $0 -a certificate authority name -u website url -c country code -s state -t town/city [-w]
+    e.g. $0 -a Peaksoft -u dev.safely.local -c NZ -s Otago -t Alexandra -w
 
-# sign this CSR with your newly created CA
-openssl x509 -req -days 500 -sha256 -in dev.safely.local.csr \
-  -CA ca.crt -CAkey ca.key -CAcreateserial -out dev.safely.local.crt
+    country, state and town are used for the certificate
+    -w  generate windows pfx file for iis. You will be prompted for a password to protect the file
+END
+  exit 1;
+}
 
-# Package your public and private key in a pkcs12 encripted file (to install with certmgr on
-# windows)
-# ref> https://www.openssl.org/docs/manmaster/apps/pkcs12.html
-openssl pkcs12 -export -inkey dev.safely.local.key -in dev.safely.local.crt -out dev.safely.local.pfx
+generate_pfx=false
+country="NZ"
+state="Otago"
+city="Alexandra"
 
+while getopts ":wa:u:c:s:t:" o; do
+  case "${o}" in
+    a)
+      authority=${OPTARG}
+      ;;
+    u)
+      url=${OPTARG}
+      ;;
+    c)
+      country=${OPTARG}
+      ;;
+    s)
+      state=${OPTARG}
+      ;;
+    t)
+      city=${OPTARG}
+      ;;
+    w)
+      generate_pfx=true
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+if [ -z "${authority}" ]; then
+  cat <<-END
+    you must specify an authority
+
+END
+  usage
+fi
+
+if [ -z "${url}" ]; then
+  cat <<-END
+    you must specify a url
+
+END
+  usage
+fi
+
+lower_authority=$(echo $authority | awk '{print tolower($0)}')
+
+if [ ! -f ${lower_authority}.crt ]; then
+  cat <<-END
+    CA file ${lower_authority}.crt not found
+    you need to find the CA files or generate a new CA
+
+END
+usage
+fi
+
+if [ ! -f ${lower_authority}.key ]; then
+  cat <<-END
+    CA file ${lower_authority}.key not found
+    you need to find the CA files or generate a new CA
+
+END
+  usage
+fi
+
+echo "creating key for ${url} into ${url}.key"
+openssl genrsa -out ${url}.key 2048
+
+echo "using new key to sign CSR (Certificate Signing Request) ${url}.key"
+openssl req -new -key ${url}.key -out ${url}.csr \
+  -subj "/C=${country}/ST=${state}/L=${city}/O=${authority}/CN=${url}"
+
+echo "signing CSR with CA ${lower_authority}.crt and {$lower_authority}.key into ${url}.crt"
+openssl x509 -req -days 500 -sha256 -in ${url}.csr \
+  -CA ${lower_authority}.crt -CAkey ${lower_authority}.key -CAcreateserial -out ${url}.crt
+
+if $generate_pfx; then
+  echo "packaging public and private key in pkcs12 encrypted file ${url}.pfx (to install with certmgr)"
+  # ref> https://www.openssl.org/docs/manmaster/apps/pkcs12.html
+  openssl pkcs12 -export -inkey ${url}.key -in ${url}.crt -out ${url}.pfx
+fi
 
